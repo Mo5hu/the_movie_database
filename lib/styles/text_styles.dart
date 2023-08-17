b1f699cff0bf1f311f8d62a346133c603a5197f9
mo5hu <k173634@nu.edu.pk>
2023-08-17 15:28:12 +0500

    Feat: Added Text Style -global

---
+++ lib/styles/text_styles.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static const TextStyle heading1 = TextStyle(
+    fontSize: 22,
+    fontWeight: FontWeight.bold,
+    color: Colors.white,
+  );
+
+  static const TextStyle heading2 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+    color: Colors.white,
+  );
+
+  static const TextStyle bodyRegular =
+      TextStyle(fontSize: 14, color: Color.fromARGB(255, 81, 81, 101));
+
+  static const TextStyle movieRow = TextStyle(
+      fontSize: 12,
+      fontWeight: FontWeight.bold,
+      color: Color.fromARGB(255, 81, 81, 101));
+
+  static const TextStyle bodySmall =
+      TextStyle(fontSize: 10, color: Color.fromARGB(255, 81, 81, 101));
+
+  static const TextStyle bodyBold = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.bold,
+    color: Colors.white,
+  );
+
+  // Add more text styles as needed
+}
