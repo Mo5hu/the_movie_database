d3c44b189188b83127561971d5e61c7122ded82b
mo5hu <k173634@nu.edu.pk>
2023-08-17 15:24:14 +0500

    feat: added theme -global

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:the_movie_database/bloc/NavigationBloc/bloc.dart';
-import 'package:the_movie_database/screens/home_screen.dart';
+import 'package:the_movie_database/constants/app_colors.dart';
 import 'package:the_movie_database/screens/hydrated_app.dart';
-import 'package:the_movie_database/screens/movie_detail_screen.dart';
-import 'package:the_movie_database/screens/saved_movie_screen.dart';
-import 'package:the_movie_database/screens/search_movie_screen.dart';
 import 'package:the_movie_database/services/api_service.dart';
+import 'package:the_movie_database/styles/text_styles.dart';
 
 void main() {
   final apiService = ApiService();
@@ -25,18 +23,21 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'The Movie Db',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+          primarySwatch: AppColors.primaryColor,
+          cardColor: AppColors.accentColor,
+          textTheme: const TextTheme(
+              displayLarge: AppTextStyles.heading1,
+              displayMedium: AppTextStyles.heading2,
+              bodyLarge: AppTextStyles.bodyBold,
+              bodyMedium: AppTextStyles.bodyRegular,
+              bodySmall: AppTextStyles.bodySmall,
+              titleMedium: AppTextStyles.movieRow)),
       initialRoute: '/',
       routes: {
         '/': (context) => BlocProvider.value(
               value: navigationBloc,
               child: const HydratedApp(),
             ),
-        '/home': (context) => const HomeScreen(),
-        '/saved': (context) => const SavedMovieScreen(),
-        '/search': (context) => const SearchMovieScreen(),
-        '/movie-detail': (context) => const MovieDetailScreen(),
       },
     );
   }
