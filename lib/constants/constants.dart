987178a4b4eced57d1ca0eee96fc133b76b32a7a
mo5hu <k173634@nu.edu.pk>
2023-08-17 15:26:27 +0500

    feat: added constants

---
+++ lib/constants/constants.dart
@@ -0,0 +1,5 @@
+// ignore_for_file: constant_identifier_names
+
+const APIKEY = '7e3913adfe4ca90b379df13059f5d8e2';
+const ACCESSTOKEN =
+    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZTM5MTNhZGZlNGNhOTBiMzc5ZGYxMzA1OWY1ZDhlMiIsInN1YiI6IjY0ZGM3NDdkYjc3ZDRiMTE0MWY3ZjdiMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Gedp4nBM3Dfu1oWF7KvBJE54-ygL_ynOr-vTb3XPgAo';
